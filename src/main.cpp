1aebc58c8b044a68f20f95ed441e97185b6eae99
NJL7502L <g.hana1211.tako@gmail.com>
2020-10-11 18:46:24 +0900

    canbusマネジメントLibraryのデバックを開始

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
-#include <Arduino.h>
+#include "Arduino.h"
 
-void setup() {}
+#include "../lib/FlexCAN_T4/CanBus/CanBus.h"
 
-void loop() {}
+CanBus<CAN0> &canbus = CanBus<CAN0>::getInstance();
+
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  canbus.begin(1000000);
+}
+
+void loop() {
+  digitalWrite(ledPin, !digitalRead(ledPin));
+
+  delay(1);
+}
